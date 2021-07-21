40429eedd82a5f769c25ad80bbdc0eb8c0247a3c
Maxime Lassiaz <maxime.lassiaz@gmail.com>
2021-07-21 11:26:37 +0200

    first commit

---
+++ routes/parking/parkingmeter.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+// Get all
+router.get('/', (req, res) => {
+
+})
+
+// Get one
+router.get('/:id', (req, res) => {
+    
+})
+
+// Create one
+router.post('/', (req, res) => {
+    
+})
+
+// Updating one
+router.patch('/:id', (req, res) => {
+    
+})
+
+// Delete one
+router.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const parkingsRouter = require('./routes/parking/parkingmeter')
+
+app.use('/parking/parkingmeter', parkingsRouter)
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000.');
+})
